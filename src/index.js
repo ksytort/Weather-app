ece3db5715ed9ea2fc02d9089a9d0041dce2efaa
ksytort <114922343+ksytort@users.noreply.github.com>
2023-04-08 21:27:51 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -53,7 +53,7 @@ function displayForecast(response) {
 
   let forecastHTML = `<div class="row">`;
   forecast.forEach(function (forecastDay, index) {
-    if (index < 6) {
+    if (index < 7) {
       forecastHTML =
         forecastHTML +
         `
